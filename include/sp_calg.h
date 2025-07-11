3560b26720505fd2b04f70ae646b85e8c8ab823f
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-07-11 15:52:30 +0700

    init

--- include/sp_calg.h
+++ include/sp_calg.h
@@ -8,31 +8,39 @@ extern "C" {
 #include <string.h>
 #include <stdlib.h>
 
-#ifndef  UNIX_LINUX
-	#ifndef __STATIC_SIMPLE_CALGORITHMS_NET__
-		#ifdef EXPORT_DLL_API_SIMPLE_CALGORITHMS_NET
-			#define DLL_API_SIMPLE_CALGORITHMS_NET		__declspec(dllexport)
-		#else
-			#define DLL_API_SIMPLE_CALGORITHMS_NET		__declspec(dllimport)
-		#endif
-	#else
-		#define DLL_API_SIMPLE_CALGORITHMS_NET
-	#endif
+#ifndef UNIX_LINUX
+#ifndef __STATIC_SIMPLE_CALGORITHMS_NET__
+#ifdef EXPORT_DLL_API_SIMPLE_CALGORITHMS_NET
+#define DLL_API_SIMPLE_CALGORITHMS_NET __declspec(dllexport)
 #else
-	#define DLL_API_SIMPLE_CALGORITHMS_NET
-#endif /*! UNIX_LINUX */ 
+#define DLL_API_SIMPLE_CALGORITHMS_NET __declspec(dllimport)
+#endif
+#else
+#define DLL_API_SIMPLE_CALGORITHMS_NET
+#endif
+#else
+#define DLL_API_SIMPLE_CALGORITHMS_NET
+#endif /*! UNIX_LINUX */
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
-typedef int (*SP_CCALLBACK_FUNCTION)(void*);
+typedef int (*SP_CCALLBACK_FUNCTION)(void *);
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
-DLL_API_SIMPLE_CALGORITHMS_NET int 
+DLL_API_SIMPLE_CALGORITHMS_NET int
 sp_calg_hello();
 
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
-#define sp_is_palindrome(__arr__, __n__, __rs__) \
-do { __rs__ = 1; \
-;\
-;\
-} while(0);
+#define sp_is_palindrome(__arr__, __n__, __rs__)                               \
+	do {                                                                   \
+		int start = 0, end = __n__ - 1;                                \
+		__rs__ = 1;                                                    \
+		while (start < end) {                                          \
+			if (str[start] != str[end]) {                          \
+				__rs__ = 0;                                    \
+				break;                                         \
+			}                                                      \
+			start++;                                               \
+			end--;                                                 \
+		};                                                             \
+	} while (0);
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
 /*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+*/
 
