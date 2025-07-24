368eba2484d1003aac85053db14d3e3967a769ad
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-07-24 09:51:08 +0700

    init

--- x-thinkcell/main.cpp
+++ x-thinkcell/main.cpp
@@ -807,7 +807,7 @@ longest_increasing_subsequence(const std::vector<T> &nums)
 //The principle is to make the last element of the list become smaller and length become larger
 //2       3       7       18      101
 int
-main()
+maindf()
 {
 #if 1
 	std::vector<double> data = { 10, 9, 2, 5, 2.5, 7, 101, 101, 101, 18, 101, 101, 101, 101, 101, 101, 
@@ -819,4 +819,66 @@ main()
 	std::cout << "Length of LIS: " << longest_increasing_subsequence(data)
 		  << '\n';
 	return 0;
+}
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+template <typename T>
+using mycoord = std::pair<int, int>;
+template <typename T>
+
+int
+SubarraySumEqualsK(std::vector<T> &vec, T &k, std::vector <mycoord<int>> &output) 
+	requires std::is_arithmetic_v<T>
+{
+	std::unordered_map<T, int> umap;
+	T sum = 0;
+	int i = 0;
+	for (auto &v : vec) {
+		sum += v;
+		if (sum == k) {
+			output.emplace_back(0, i);
+		}
+		if (umap.find(sum - k) != umap.end()) {
+			int j = i;
+			T sumj = 0;
+			for (; j >= 0; --j) {
+				sumj += vec[j];
+				if (sumj == k) {
+					output.emplace_back(j, i);
+					break;
+				}
+			}
+		}
+		umap[sum]++;
+		++i;
+	}
+	return 0;
+}
+
+int
+test_SubarraySumEqualsK()
+{
+#if 0
+	std::vector<double> data = {10, 9, 2, 5, 2.5, 7, 101, 101, 101, 18, 101,
+	    101, 101, 101, 101, 101, 15, 16, 17, 19, 20, 1, 2, 3, 4, 5, 6, 7,
+	    7.1, 7.2, 8, 9.0};
+	// std::vector<float> data = {10, 9, 2, 5, 2.5, 7, 101, 18, 101, 101,
+	// 15, 16, 17, 19, 20, 1, 2, 3, 4, 5, };
+#else
+	std::vector<int> data = {7, 0, -7, 7, 3, 7, 101, 18, 101, 101, 15};
+#endif
+	std::vector<mycoord<int>> output;
+	int k = 7;
+	int ret = SubarraySumEqualsK(data, k, output);
+	return ret;
+}
+
+int
+main()
+{
+	return test_SubarraySumEqualsK();
 }
