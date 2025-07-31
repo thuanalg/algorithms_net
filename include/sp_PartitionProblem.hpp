6054dfb160fd225c7d55964194113159a9208a53
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-07-31 14:00:18 +0700

    init

--- include/sp_PartitionProblem.hpp
+++ include/sp_PartitionProblem.hpp
@@ -14,23 +14,86 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <numeric>   // C++98 (ISO/IEC 14882:1998)
 using namespace std;
 
 
-template <typename T> using ProfitCoord = std::pair<int, int>;
-template <typename T>
+//template <typename T> using ProfitCoord = std::pair<int, int>;
+//template <typename T>
 inline int
-sp_PartitionProblem(
-    std::vector<T> &vec, T &output, ProfitCoord<int> &index)
-	requires std::is_arithmetic_v<T>
+sp_PartitionProblem( std::vector<int> &arr, bool &result, int kVal)
 {
+	int ret = 0;
+	// Calculate sum of the elements in array
+	int sum = std::accumulate(arr.begin(), arr.end(), 0);
 
-	return 0;
+	// If sum is odd, there cannot be two
+	// subsets with equal sum
+	if (sum % 2 != 0) {
+		result = false;
+		return ret;
+	}
+
+	//sum = sum / 2;
+	//sum += 10;
+
+	int n = arr.size();
+	vector<bool> prev(sum + 1, false), curr(sum + 1);
+
+	// Mark prev[0] = true as it is true
+	// to make sum = 0 using 0 elements
+	prev[0] = true;
+
+	// Fill the subset table in
+	// bottom up manner
+
+	for (int i = 1; i <= n; i++) {
+		for (int j = 0; j <= sum; j++) {
+			if (j < arr[i - 1]) {
+				curr[j] = prev[j];
+			} else {
+				int k = j - arr[i - 1];
+				curr[j] = (prev[j] || prev[j - arr[i - 1]]);
+				bool tmp = curr[j];
+				int a = 0;
+			}
+#if 0
+			std::cout << "i = " << i << ", a[" << i - 1
+				  << "] = " << arr[i - 1] << "\t\t";
+			for (auto v : curr) {
+				std::cout << (v ? "X" : "-") << "\t";
+			}
+			std::cout << "\n";
+#endif
+		}
+#if 1
+		std::cout << "i = " << i << ", a[" << i - 1
+			  << "] = " << arr[i - 1] << "\t\t";
+		for (auto v : curr) {
+			std::cout << (v ? "X" : "-") << "\t";
+		}
+		std::cout << "\n";
+#endif
+		prev = curr;
+	}
+	result = curr[kVal];
+	return ret;
 }
 
 inline int
 sp_PartitionProblem_test()
 {
+	bool result = false;
+	int kVal = 4;
+	// vector<int> arr = {1, 2,  2, 5};
+	vector<int> vec = {5, 2, 1};
+	sp_PartitionProblem(vec, result, kVal);
+
+	if (result) {
+		cout << "True" << endl;
+	} else {
+		cout << "False" << endl;
+	}
 	return 0;
 }
 #endif // __sp_PartitionProblem__
