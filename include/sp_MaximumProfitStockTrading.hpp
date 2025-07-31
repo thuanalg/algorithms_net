27ecba7604664c0addd90c0c59708072a030127a
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-07-31 13:36:13 +0700

    init

--- include/sp_MaximumProfitStockTrading.hpp
+++ include/sp_MaximumProfitStockTrading.hpp
@@ -16,9 +16,9 @@
 #include <unordered_map>
 using namespace std;
 
+
 template <typename T> using ProfitCoord = std::pair<int, int>;
 template <typename T>
-
 inline int
 sp_MaximumProfitStockTrading(
     std::vector<T> &vec, T &output, ProfitCoord<int> &index)
@@ -29,85 +29,11 @@ sp_MaximumProfitStockTrading(
 	}
 	T min_price = vec[0], max_profit = vec[0] - vec[0];
 	int i = 0;
-	int start = -1;
-	int end = -1;
 	ProfitCoord<int> prev(-1, -1), curr(-1, -1);
 	for (const auto &v : vec) {
 		if (i == 0) {
-			start = 0;
-			end = 0;
-			prev.first = i;
-			curr.first = i;			
-			min_price = v;
-			max_profit = (v - v);
-			++i;
-			continue;
-		}
-		if (v < min_price) {
-			min_price = v;
-			if (prev.first < 0) {
-				prev.first = i;
-				curr.first = i;
-			} else if (curr.first == prev.first) {
-				curr.first = i;
-			} else if (curr.first > prev.first) {
-				// if ()
-				if (curr.first < curr.second) {
-					prev.first = curr.first;
-				}
-				curr.first = i;
-			}
-		} else if (v - min_price > max_profit) {
-			max_profit = (v - min_price);
-			end = i;
-			if (prev.second < 0) {
-				prev.second = i;
-				curr.second = i;
-			} else if (prev.second == curr.second) {
-				curr.second = i;
-			} else if (prev.second < curr.second) {
-				prev.second = curr.second;
-				curr.second = i;
-			}
-		}
-		++i;
-	}
-	output = max_profit;
-	if (curr.second > curr.first) {
-		index = curr;
-	} 
-	else if (curr.second > prev.first) {
-		index.first = prev.first;
-		index.second = curr.second;
-	} else {
-		index.first = -1;
-		index.second = -1;
-	}
-	return 0;
-}
-
-template <typename T> using ProfitCoord = std::pair<int, int>;
-template <typename T>
-inline int
-sp_MaximumProfitStockTrading_1(
-    std::vector<T> &vec, T &output, ProfitCoord<int> &index)
-	requires std::is_arithmetic_v<T>
-{
-	if (vec.size() < 2) {
-		return 0;
-	}
-	T min_price = vec[0], max_profit = vec[0] - vec[0];
-	int i = 0;
-	ProfitCoord<int> prev(-1, -1), curr(-1, -1);
-	for (const auto &v : vec) {
-		if (i == 0) {
-
-			prev.first = i;
-			curr.first = i;
-
-			prev.second = i;
-			curr.second = i;
-
+			prev.first = prev.second = i;
+			curr = prev;
 			min_price = v;
 			max_profit = (v - v);
 			++i;
@@ -128,9 +54,13 @@ sp_MaximumProfitStockTrading_1(
 	output = max_profit;
 	if (curr.second > curr.first) {
 		index = curr;
+		output = vec[index.second] - vec[index.first];
 	} else {
 		index = prev;
+		output = vec[index.second] - vec[index.first];
 	}
+#if 0
+#endif
 	return 0;
 }
 
@@ -148,10 +78,10 @@ sp_MaximumProfitStockTrading_test()
 #else
 	double k = 0;
 	std::vector<double> data = {
-	    7, 0, 7, 7, 3, 7, 101, 18, 10, 101, 101, -1, 101, -2};
+	    7, 0, 7, 7, 3, 7, 101, 18, 10, -2, 101, -9};
 #endif
 
-	int ret = sp_MaximumProfitStockTrading_1(data, k, index);
+	int ret = sp_MaximumProfitStockTrading(data, k, index);
 	return ret;
 }
 #endif // __sp_MaximumProfitStockTrading__
