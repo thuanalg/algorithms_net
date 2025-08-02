7aec9a75aafcd506e1f25ebf4d2a95ee8933056e
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-08-02 15:18:37 +0700

    init

--- include/sp_SlidingWindowMinimumMaximum.hpp
+++ include/sp_SlidingWindowMinimumMaximum.hpp
@@ -18,24 +18,23 @@ using namespace std;
 
 template <typename T>
 inline int
-sp_SlidingWindowMaximum( std::vector<T> &arr, int k)
+sp_SlidingWindowMaximum(std::vector<T> &vec, int k, vector<T> &output)
 	requires std::is_arithmetic_v<T>
 {
 	int ret = 0;
 	std::deque<T> dq;
-	vector<T> res;
 
-	for (int i = 0; i < arr.size(); ++i) {
+	for (int i = 0; i < vec.size(); ++i) {
 		if (!dq.empty() && dq.front() <= i - k)
 			dq.pop_front();
 
-		while (!dq.empty() && arr[dq.back()] <= arr[i])
+		while (!dq.empty() && vec[dq.back()] <= vec[i])
 			dq.pop_back();
 
 		dq.push_back(i);
 
 		if (i >= k - 1)
-			res.push_back(arr[dq.front()]);
+			output.push_back(vec[dq.front()]);
 	}
 
 	//return res;
@@ -44,24 +43,23 @@ sp_SlidingWindowMaximum( std::vector<T> &arr, int k)
 
 template <typename T>
 inline int
-sp_SlidingWindowMinimum(std::vector<T> &arr, int k)
+sp_SlidingWindowMinimum(std::vector<T> &vec, int k, vector<T> &output)
 	requires std::is_arithmetic_v<T>
 {
 	int ret = 0;
 	deque<T> dq;
-	vector<T> res;
 	
-	for (int i = 0; i < arr.size(); ++i) {
+	for (int i = 0; i < vec.size(); ++i) {
 		if (!dq.empty() && dq.front() <= i - k)
 			dq.pop_front();
 	
-		while (!dq.empty() && arr[dq.back()] >= arr[i])
+		while (!dq.empty() && vec[dq.back()] >= vec[i])
 			dq.pop_back();
 	
 		dq.push_back(i);
 	
 		if (i >= k - 1)
-			res.push_back(arr[dq.front()]);
+			output.push_back(vec[dq.front()]);
 	}
 	
 	return 0;
@@ -71,7 +69,10 @@ inline int
 sp_SlidingWindowMinimumMaximum_test()
 {
 	std::vector<int> vec = { 5, 7, 1, 4, 3, 6, 2, 9, 2 };
-	sp_SlidingWindowMaximum(vec, 3);
+	std::vector<int> output;
+	sp_SlidingWindowMaximum(vec, 3, output);
+	output.clear();
+	sp_SlidingWindowMinimum(vec, 3, output);
 	return 0;
 }
 #endif // __sp_SlidingWindowMinimumMaximum__
