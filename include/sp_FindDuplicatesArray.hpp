637983aec69b563fd0c6ea3c69dce7b7a9f1e439
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-08-02 07:57:07 +0700

    init

---
+++ include/sp_FindDuplicatesArray.hpp
@@ -0,0 +1,36 @@
+﻿#ifndef __sp_FindDuplicatesArray__
+#define __sp_FindDuplicatesArray__
+
+#include <type_traits> // C++11
+#include <concepts> // C++20
+#include <ranges> // C++20
+#include <algorithm>
+#include <vector>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+#include <map>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+template <typename T> using EqualKcoord = std::pair<T, T>;
+
+template <typename T>
+inline int
+sp_FindDuplicatesArray( std::vector<T> &vec, T &k, std::vector<EqualKcoord<int>> &output)
+	requires std::is_arithmetic_v<T>
+{
+
+	return 0;
+}
+
+
+inline int
+sp_FindDuplicatesArray_test()
+{
+	return 0;
+}
+#endif // __sp_FindDuplicatesArray__
