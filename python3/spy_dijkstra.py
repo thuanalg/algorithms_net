587758d9d14afa44315ec7816def1a9c17b11419
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-08-05 17:10:35 +0700

    init

--- python3/spy_dijkstra.py
+++ python3/spy_dijkstra.py
@@ -1,3 +1,27 @@
-﻿def spy_dijkstra_test():
-    print("test")
+﻿import sys
+import heapq
+
+INF = sys.maxsize
+def spy_dijkstra(grid, i, parent, output):
+    print("--")
+    n = len(grid)
+    dist = [INF] * n
+    pq = []
+
+def spy_dijkstra_test():
+    grid = [
+        [0, 1.2, 4, INF, INF, 10],     # 0
+        [INF, 0, 1, INF, 1, INF],      # 1
+        [INF, INF, 0, 1, 1, 12],       # 2
+        [INF, INF, INF, 0, INF, 2],    # 3
+        [INF, INF, INF, 10, 0, 6],     # 4
+        [INF, INF, INF, INF, INF, 0]   # 5
+    ]
+    
+    for row in grid:
+        print(row)
+    parent = []
+    output = []
+    spy_dijkstra(grid, 0, parent, output)
+
 spy_dijkstra_test()
