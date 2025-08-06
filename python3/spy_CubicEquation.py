1de8e384361a2ca3c74148b3da85eac9943ab38d
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-08-06 15:25:57 +0700

    init

--- python3/spy_CubicEquation.py
+++ python3/spy_CubicEquation.py
@@ -2,23 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Cubic function: x^3 + 11x - 1
-def cubic(x):
-    return x**3  - 100*x 
+def cubic(x, c):
+    return x**3  + c*x 
 
 # Generate x values from -2 to 5
+c = -100
 x = np.linspace(-10, 10, 5000)
-y = cubic(x)
+y = cubic(x, c)
 
 # Compute real roots of the cubic equation
-roots = np.roots([1, 0, -100, 0])  # coefficients [a,b,c,d]
+roots = np.roots([1, 0, c, 0])  # coefficients [a,b,c,d]
 real_roots = roots[np.isreal(roots)].real  # keep only real roots
 
 # Plot the cubic function
 plt.figure(figsize=(8, 5))
-plt.plot(x, y, label=r'$y = x^3 - 100x $')
+plt.plot(x, y, label=r'$y = x^3 + cx $')
 
 # Highlight the roots
-plt.scatter(real_roots, cubic(real_roots), color='red', zorder=5, label='Roots')
+plt.scatter(real_roots, cubic(real_roots, a), color='red', zorder=5, label='Roots')
 
 # Draw X and Y axes
 plt.axhline(0, color='black', linewidth=1)
