eda0651f679d401fac8f93727c80dd1c1a5d2c95
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-08-10 11:01:17 +0700

    init

--- xtmp/main.c
+++ xtmp/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define M_DEGREE 2
+typedef struct BTreeNode{
+	int idkey;
+    int num_keys; // Number of keys currently in the node
+    int keys[M_DEGREE-1]; // Array of keys
+    struct BTreeNode *children[M_DEGREE]; // Array of child pointers
+    int is_leaf; // True if node is a leaf	
+} BTreeNode;
 int main(int argc, char *argv[])
 {
 	int a = 0;
