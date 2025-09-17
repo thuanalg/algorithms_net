eaaa2ae70dcf4640339b8edcf2198175c04a6695
Thuận Nguyễn-Thái <nguyenthaithuanalg@gmail.com>
2025-09-17 17:11:39 +0700

    init

---
+++ xffmpeg/src/FFWrapper.h
@@ -0,0 +1,63 @@
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/
+
+/* Email:
+ *		<nguyenthaithuanalg@gmail.com> - Nguyễn Thái Thuận
+ * Mobile:
+ *		<+084.799.324.179>
+ * Skype:
+ *		<nguyenthaithuanalg>
+ * Date:
+ *		<2025-Aug-14>
+ * The lasted modified date:
+ * Decription:
+ *		
+*/
+
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/
+#ifndef ___FF_WRAPPER__
+#define ___FF_WRAPPER__
+
+
+#if 0
+#ifndef UNIX_LINUX
+#define UNIX_LINUX                      
+#endif
+#endif
+
+#if 0
+#ifndef __MACH__
+#define __MACH__                        
+#endif
+#endif
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+
+#ifndef UNIX_LINUX
+#ifndef __STATIC_FF_WRAPPER__
+#ifdef EXPORT_DLL_API_FF_WRAPPER
+#define DLL_API_FF_WRAPPER              __declspec(dllexport)
+#else
+#define DLL_API_FF_WRAPPER              __declspec(dllimport)
+#endif
+#else
+#define DLL_API_FF_WRAPPER              
+#endif
+#else
+#define DLL_API_FF_WRAPPER              
+#endif /*! UNIX_LINUX */
+
+
+/*+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-*/
+
+/* DLL_API_FF_WRAPPER */
+DLL_API_FF_WRAPPER int
+ffwr_all_codecs();
+#ifdef __cplusplus
+}
+#endif
+#endif
