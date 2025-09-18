210c7f095b9f851a178290fd2fc862c00b202201
Thuận Nguyễn-Thái <nguyenthaithuanalg@gmail.com>
2025-09-18 16:02:48 +0700

    init

--- xffmpeg/main.c
+++ xffmpeg/main.c
@@ -7,7 +7,7 @@
 #include <libswscale/swscale.h>
 
 int save_frame_to_mp4(AVFrame *frame, const char *filename) ;
-
+//int save_frame_to_mp4(AVFrame *frame, const char *filename) ;
 #define UNIX_LINUX
 
 int main(int argc, char* argv[]) {
@@ -21,8 +21,13 @@ int main(int argc, char* argv[]) {
 #endif
     const AVInputFormat *ifmt = NULL;
     while ((ifmt = av_input_video_device_next(ifmt))) {
-        printf("Input format: %s (%s)\n", ifmt->name, ifmt->long_name);
+        printf("Video Input format: %s (%s)\n", ifmt->name, ifmt->long_name);
     }    
+    fprintf(stdout , "\n\n");
+    while ((ifmt = av_input_audio_device_next(ifmt))) {
+        printf("Audio Input format: %s (%s)\n", ifmt->name, ifmt->long_name);
+    }        
+    //return 1;
     avdevice_register_all();
     avformat_network_init();
     //get_all_encoders();
@@ -72,6 +77,10 @@ int main(int argc, char* argv[]) {
     for (unsigned i = 0; i < fmt_ctx->nb_streams; i++) {
         if (fmt_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
             video_index = i;
+            AVCodec *video = fmt_ctx->video_codec;
+            if(video) {
+                fprintf(stdout, "video codec name: %s\n", video->name);
+            }
             break;
         }     
     }
@@ -79,6 +88,10 @@ int main(int argc, char* argv[]) {
 
         if (audio_ctx->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
             audio_index = i;
+            AVCodec *audio = audio_ctx->audio_codec;
+            if(audio) {
+                fprintf(stdout, "audio codec name: %s\n", audio->name);
+            }
             break;
         }        
     }    
@@ -101,6 +114,15 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
+     //Prepare codec to decode audio
+    AVCodecParameters *codecpar_audio = audio_ctx->streams[audio_index]->codecpar;
+    AVCodec *codec_audio = avcodec_find_decoder(codecpar_audio->codec_id);
+    if (!codec_audio) {
+        fprintf(stderr, "Not found\n");
+        avformat_close_input(&audio_ctx);
+        return 1;
+    }
+
     AVCodecContext *codec_ctx = avcodec_alloc_context3(codec);
     avcodec_parameters_to_context(codec_ctx, codecpar);
     if (avcodec_open2(codec_ctx, codec, NULL) < 0) {
@@ -110,14 +132,60 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    AVPacket *pkt = av_packet_alloc();
-    AVFrame *frame = av_frame_alloc();
+    AVCodecContext *audio_codec_ctx = avcodec_alloc_context3(codec_audio);
+    avcodec_parameters_to_context(audio_codec_ctx, codecpar_audio);
+    if (avcodec_open2(audio_codec_ctx, codec_audio, NULL) < 0) {
+        fprintf(stderr, "Cannot open codec\n");
+        avcodec_free_context(&audio_codec_ctx);
+        avformat_close_input(&audio_ctx);
+        return 1;
+    }    
 
-    while (av_read_frame(fmt_ctx, pkt) >= 0) {
-        if (pkt->stream_index == video_index) {
+    AVPacket *pkt = av_packet_alloc();
+    AVPacket *audio_pkt = av_packet_alloc();
+    AVFrame *frame = av_frame_alloc();
+    int videoread = -1, audioread = -1;
+    while (1) {
+    #if 1  
+        videoread = av_read_frame(fmt_ctx, pkt);
+        if (videoread >= 0 && pkt->stream_index == video_index) {
             if (avcodec_send_packet(codec_ctx, pkt) == 0) 
             {
+                //avcodec_receive_packet
+                //fmt_ctx->pb
+                //avio_alloc_context
 			    ret = avcodec_receive_frame(codec_ctx, frame);
+                while (ret == 0) {
+                #if 0
+                    printf("Frame %d: type=%c, size=%d bytes\n",
+                           codec_ctx->frame_number,
+                           av_get_picture_type_char(frame->pict_type),
+                           frame->pkt_size);
+                    av_frame_unref(frame);
+                #else
+                    //AVSampleFormat a = -1; //unknown
+                    int pkt_size = frame->pkt_size;
+                    int fmt = frame->format;
+                    int nb_samples = frame->nb_samples;
+                    int mytype = 0;
+                    mytype = codec_ctx->codec_type;
+                    fprintf(stdout, "nb_side_data: %d, fm: %d, nb_samples: %d, mytype: %d, sze: %d\n", 
+                        frame->nb_side_data, fmt, nb_samples, mytype, pkt_size);
+                    av_frame_unref(frame);
+                #endif
+                }
+            }
+        }
+
+        av_packet_unref(pkt);
+#else
+        audioread = av_read_frame(audio_ctx, audio_pkt);
+        int stream_index = audio_pkt->stream_index ;
+        if (audioread >= 0 && stream_index == audio_index) {
+            stream_index = avcodec_send_packet(audio_codec_ctx, audio_pkt);
+            if (stream_index == 0) 
+            {
+			    ret = avcodec_receive_frame(audio_codec_ctx, frame);
                 while (ret == 0) {
                 #if 0
                     printf("Frame %d: type=%c, size=%d bytes\n",
@@ -130,15 +198,47 @@ int main(int argc, char* argv[]) {
                     int fmt = frame->format;
                     int nb_samples = frame->nb_samples;
                     int mytype = 0;
-                    mytype = codec_ctx->codec_type;
-                    fprintf(stdout, "nb_side_data: %d, fm: %d, nb_samples: %d, mytype: %d\n", 
+                    mytype = audio_codec_ctx->codec_type;
+                    fprintf(stdout, "nb_side_data: %d, fmt: %d, nb_samples: %d, mytype: %d\n", 
                         frame->nb_side_data, fmt, nb_samples, mytype);
-                    av_frame_unref(frame);
+                    av_frame_unref(frame);                    
+#if 0                    
+                    enum AVSampleFormat {
+    AV_SAMPLE_FMT_NONE = -1,
+    AV_SAMPLE_FMT_U8,          ///< unsigned 8 bits
+    AV_SAMPLE_FMT_S16,         ///< signed 16 bits
+    AV_SAMPLE_FMT_S32,         ///< signed 32 bits
+    AV_SAMPLE_FMT_FLT,         ///< float
+    AV_SAMPLE_FMT_DBL,         ///< double
+
+    AV_SAMPLE_FMT_U8P,         ///< unsigned 8 bits, planar
+    AV_SAMPLE_FMT_S16P,        ///< signed 16 bits, planar
+    AV_SAMPLE_FMT_S32P,        ///< signed 32 bits, planar
+    AV_SAMPLE_FMT_FLTP,        ///< float, planar
+    AV_SAMPLE_FMT_DBLP,        ///< double, planar
+    AV_SAMPLE_FMT_S64,         ///< signed 64 bits
+    AV_SAMPLE_FMT_S64P,        ///< signed 64 bits, planar
+
+    AV_SAMPLE_FMT_NB           ///< Number of sample formats. DO NOT USE if linking dynamically
+};
+enum AVMediaType {
+    AVMEDIA_TYPE_UNKNOWN = -1,  ///< Usually treated as AVMEDIA_TYPE_DATA
+    AVMEDIA_TYPE_VIDEO,
+    AVMEDIA_TYPE_AUDIO,
+    AVMEDIA_TYPE_DATA,          ///< Opaque data information usually continuous
+    AVMEDIA_TYPE_SUBTITLE,
+    AVMEDIA_TYPE_ATTACHMENT,    ///< Opaque data information usually sparse
+    AVMEDIA_TYPE_NB
+};
+
+#endif
+
                 #endif
                 }
             }
         }
         av_packet_unref(pkt);
+#endif
     }
 
     av_frame_free(&frame);
@@ -416,4 +516,116 @@ Input format: kmsgrab (KMS screen capture)
 Input format: lavfi (Libavfilter virtual input device)
 Input format: video4linux2,v4l2 (Video4Linux2 device grab)
 Input format: x11grab (X11 screen capture, using XCB)
+
+--------------------------------------------------------------------------
+#include <libavformat/avformat.h>
+#include <libavcodec/avcodec.h>
+#include <libavutil/opt.h>
+#include <libavutil/imgutils.h>
+
+typedef struct {
+    AVFormatContext *fmt_ctx;
+    AVStream *stream;
+    AVCodecContext *codec_ctx;
+    AVIOContext *avio_ctx;
+    uint8_t *avio_buffer;
+    FILE *file;
+    int64_t pts;
+} MP4Writer;
+
+int mp4_writer_init(MP4Writer *w, FILE *fp, int width, int height) {
+    memset(w, 0, sizeof(*w));
+    w->file = fp;
+    w->pts = 0;
+
+    // Allocate format context
+    avformat_alloc_output_context2(&w->fmt_ctx, NULL, "mp4", NULL);
+    if (!w->fmt_ctx) return -1;
+
+    // Setup AVIO to write into FILE*
+    w->avio_buffer = av_malloc(4096);
+    w->avio_ctx = avio_alloc_context(w->avio_buffer, 4096, 1, fp, NULL, [](void *opaque, uint8_t *buf, int buf_size) {
+        return fwrite(buf, 1, buf_size, (FILE *)opaque);
+    }, NULL);
+    w->fmt_ctx->pb = w->avio_ctx;
+    w->fmt_ctx->flags |= AVFMT_FLAG_CUSTOM_IO;
+
+    // Find encoder
+    const AVCodec *codec = avcodec_find_encoder(AV_CODEC_ID_H264);
+    if (!codec) return -1;
+
+    // Create stream
+    w->stream = avformat_new_stream(w->fmt_ctx, NULL);
+    if (!w->stream) return -1;
+
+    // Create codec context
+    w->codec_ctx = avcodec_alloc_context3(codec);
+    w->codec_ctx->codec_id = AV_CODEC_ID_H264;
+    w->codec_ctx->bit_rate = 400000;
+    w->codec_ctx->width = width;
+    w->codec_ctx->height = height;
+    w->codec_ctx->time_base = (AVRational){1, 25};
+    w->codec_ctx->framerate = (AVRational){25, 1};
+    w->codec_ctx->gop_size = 12;
+    w->codec_ctx->max_b_frames = 2;
+    w->codec_ctx->pix_fmt = AV_PIX_FMT_YUV420P;
+
+    if (w->fmt_ctx->oformat->flags & AVFMT_GLOBALHEADER)
+        w->codec_ctx->flags |= AV_CODEC_FLAG_GLOBAL_HEADER;
+
+    // Open encoder
+    if (avcodec_open2(w->codec_ctx, codec, NULL) < 0) return -1;
+
+    avcodec_parameters_from_context(w->stream->codecpar, w->codec_ctx);
+    w->stream->time_base = w->codec_ctx->time_base;
+
+    // Write MP4 header
+    if (avformat_write_header(w->fmt_ctx, NULL) < 0) return -1;
+
+    return 0;
+}
+
+int mp4_writer_push_frame(MP4Writer *w, AVFrame *frame) {
+    frame->pts = w->pts++;
+
+    if (avcodec_send_frame(w->codec_ctx, frame) < 0)
+        return -1;
+
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
+    while (avcodec_receive_packet(w->codec_ctx, &pkt) == 0) {
+        pkt.stream_index = w->stream->index;
+        pkt.duration = 1;
+        av_interleaved_write_frame(w->fmt_ctx, &pkt);
+        av_packet_unref(&pkt);
+    }
+
+    return 0;
+}
+
+void mp4_writer_close(MP4Writer *w) {
+    // Flush encoder
+    avcodec_send_frame(w->codec_ctx, NULL);
+    AVPacket pkt;
+    av_init_packet(&pkt);
+    pkt.data = NULL;
+    pkt.size = 0;
+
+    while (avcodec_receive_packet(w->codec_ctx, &pkt) == 0) {
+        pkt.stream_index = w->stream->index;
+        av_interleaved_write_frame(w->fmt_ctx, &pkt);
+        av_packet_unref(&pkt);
+    }
+
+    av_write_trailer(w->fmt_ctx);
+
+    avcodec_free_context(&w->codec_ctx);
+    avformat_free_context(w->fmt_ctx);
+    av_free(w->avio_buffer);
+    avio_context_free(&w->avio_ctx);
+}
+
 #endif
