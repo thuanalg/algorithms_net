dcb4b14be01107becaeb7defa172b5895c48a707
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-10-29 22:57:09 +0700

    init

---
+++ xffmpeg/resource/include/SDL2/SDL_revision.h
@@ -0,0 +1,8 @@
+/* #undef SDL_VENDOR_INFO */
+#define SDL_REVISION_NUMBER 0
+
+#ifdef SDL_VENDOR_INFO
+#define SDL_REVISION "SDL-release-2.32.8-0-g98d1f3a45 (" SDL_VENDOR_INFO ")"
+#else
+#define SDL_REVISION "SDL-release-2.32.8-0-g98d1f3a45"
+#endif
---
+++ xffmpeg/resource/src/ffwr_render.c
@@ -0,0 +1,4 @@
+#include <simplelog.h>
+#include <ffwr_render.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_syswm.h>
