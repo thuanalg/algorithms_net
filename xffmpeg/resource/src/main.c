18023f6a2acb413d14d6adf6eff3ed1af2c8233d
Thuan Nguyen Thai <nguyenthaithuanalg@gmail.com>
2025-10-31 15:38:21 +0700

    init

--- xffmpeg/resource/src/main.c
+++ xffmpeg/resource/src/main.c
@@ -4,11 +4,20 @@
 #include <stdlib.h>
 FFWR_INPUT_ST mfcinfo;
 int main(int argc, char *argv[]) {
+	int ret = 0;
+	char cfgpath[1024] = {0};
+	SPL_INPUT_ARG input = {0};
+	snprintf(cfgpath, 1024, "z.cfg");
+	snprintf(input.folder, SPL_PATH_FOLDER, "%s", cfgpath);
+	snprintf(input.id_name, 100, "tvk_wind");
+	ret = spl_init_log_ext(&input);
+	
 	snprintf(mfcinfo.name, sizeof(mfcinfo.name), "%s", 
 		"tcp://127.0.0.1:12345");
 	ffwr_create_demux(&mfcinfo);	
 	while(1) {
 		spl_sleep(1);
 	}
+	spl_finish_log();
 	return 0;
 }
